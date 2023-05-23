403fd2e8c0137db6c02d918ae795d8ff4a77f376
Ali Jawad <32601057+alijawad07@users.noreply.github.com>
2023-05-24 00:01:31 +0500

    Update test.py

--- test.py
+++ test.py
@@ -8,12 +8,16 @@ from super_gradients.common.object_names import Models
 from super_gradients.training import models
 from tqdm import tqdm
 import shutil
+import numpy as np
+import random
 
 # Define a function to generate a unique color for each class label
 def get_color(label):
     hash_value = hash(str(label))
     # Add a large constant value to handle negative hash values
     color = [int(x) for x in str((hash_value + 2**32) % (256**3))][0:3]
+    # Ensure that the color is bright and distinct
+    color = [max(64, c) for c in color]
     return tuple(color)
 
 def parse_args():
@@ -62,9 +66,15 @@ if os.path.exists(output_dir_labels):
 os.makedirs(output_dir_images, exist_ok=True)
 os.makedirs(output_dir_labels, exist_ok=True)
 
-label_colors = {}
+# Define a dictionary to store the assigned colors for each class
+class_colors = {}
+
+# Assign random colors to each class
 for label in classes:
-    label_colors[label] = get_color(label)
+    # Generate a random color (RGB values)
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    # Assign the color to the class
+    class_colors[label] = color
 
 label_names = {i: label for i, label in enumerate(classes)}
 
@@ -89,6 +99,7 @@ for filename in tqdm(os.listdir(args.source)):
         # Open the output file for writing
         with open(output_file, 'w') as f:
             frame_count = 0
+            start_time = time.time()
 
             while True:
                 # Read a frame from the video
@@ -120,6 +131,7 @@ for filename in tqdm(os.listdir(args.source)):
 
                             # Draw the bounding box and label
                             x1, y1, x2, y2 = [int(coord) for coord in bboxes_xyxy[i]]
+                            
 
                             boxes.append([x1, y1, x2, y2])
                             scores.append(float(confidence[i]))
@@ -128,18 +140,42 @@ for filename in tqdm(os.listdir(args.source)):
 
                 # Draw the bounding boxes and class labels on the image
                 for box, score, label in zip(boxes, scores, labels_int):
-                    
                     x1, y1, x2, y2 = box
-                    color = get_color(label)
+
+                    # Retrieve the color for the class label
+                    color = class_colors[label]
+
+                    # Adjust the thickness of the bounding box and text
+                    thickness = 1
+
                     label_int = None
+                    
                     for k, v in label_names.items():
                         if v == label:
                             label_int = k
                             break
-                    class_name = label_names.get(label, 'unknown')
-                    
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, thickness=2)
-                    cv2.putText(frame, f'{class_name}:{score:.2f}', (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                    class_name = label_names.get(label_int, 'unknown')
+
+                    # Draw the bounding box
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, thickness)
+
+                    # Define the font properties
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    font_scale = 0.5
+                    font_thickness = 1
+
+                    # Calculate the text size for better placement
+                    (text_width, text_height), _ = cv2.getTextSize(f'{class_name}:{score:.2f}', font, font_scale, font_thickness)
+
+                    # Adjust the text position to avoid overlap
+                    text_x = x1
+                    text_y = y1 - 10 if y1 >= 20 else y1 + text_height + 10
+
+                    # Draw a filled rectangle as the background for better readability
+                    cv2.rectangle(frame, (x1, text_y - text_height), (x1 + text_width, text_y), color, -1)
+
+                    # Draw the class label and score
+                    cv2.putText(frame, f'{class_name}:{score:.2f}', (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness)
                       
                 
                 # Convert the image to PNG format
@@ -157,6 +193,11 @@ for filename in tqdm(os.listdir(args.source)):
                     f.write(f"{int(labels[i])} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n")
 
                 frame_count += 1
+                
+                # Calculate and print FPS
+                elapsed_time = time.time() - start_time
+                fps = frame_count / elapsed_time
+                cv2.putText(frame, f'FPS: {fps:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
                 # Write the frame to the output video
                 out_video.write(frame)
@@ -203,23 +244,45 @@ for filename in tqdm(os.listdir(args.source)):
                         boxes.append([x1, y1, x2, y2])
                         scores.append(float(confidence[i]))
                         labels_int.append(label)
-
-
+   
             # Draw the bounding boxes and class labels on the image
             for box, score, label in zip(boxes, scores, labels_int):
-                
                 x1, y1, x2, y2 = box
-               
-                color = get_color(label)
+
+                # Retrieve the color for the class label
+                color = class_colors[label]
+                
+                # Adjust the thickness of the bounding box and text
+                thickness = 1
+
                 label_int = None
+                
                 for k, v in label_names.items():
                     if v == label:
                         label_int = k
                         break
                 class_name = label_names.get(label_int, 'unknown')
-               
-                cv2.rectangle(image, (x1, y1), (x2, y2), color, thickness=2)
-                cv2.putText(image, f'{class_name}:{score:.2f}', (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+                # Draw the bounding box
+                cv2.rectangle(image, (x1, y1), (x2, y2), color, thickness)
+
+                # Define the font properties
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_scale = 0.5
+                font_thickness = 1
+
+                # Calculate the text size for better placement
+                (text_width, text_height), _ = cv2.getTextSize(f'{class_name}:{score:.2f}', font, font_scale, font_thickness)
+
+                # Adjust the text position to avoid overlap
+                text_x = x1
+                text_y = y1 - 10 if y1 >= 20 else y1 + text_height + 10
+
+                # Draw a filled rectangle as the background for better readability
+                cv2.rectangle(image, (x1, text_y - text_height), (x1 + text_width, text_y), color, -1)
+
+                # Draw the class label and score
+                cv2.putText(image, f'{class_name}:{score:.2f}', (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness)
                 
             # Save the image with bounding boxes to the output directory
             output_path = os.path.join(output_dir_images, filename)
